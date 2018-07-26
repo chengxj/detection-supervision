34c4d13acde4c5da425fbda05f7048be6fd58f21
chengxj <chengxj_1986@126.com>
2018-07-26 11:39:23 +0800

    添加功能组件的支持
    添加功能组件的支持

--- src/main/java/com/ultrapower/detection/supervision/Application.java
+++ src/main/java/com/ultrapower/detection/supervision/Application.java
@@ -1,13 +1,19 @@
 package com.ultrapower.detection.supervision;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	private final static Logger LOG = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
+		LOG.info("==================================");
 		SpringApplication.run(Application.class, args);
+		LOG.info("==================================");
 	}
 	
 }
